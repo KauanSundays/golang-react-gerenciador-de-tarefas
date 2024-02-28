3332e316c82946be380a7ec8c0a4ce4d50111d33
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-02-27 21:09:24 -0300

    wip

--- client/src/Tarefas.js
+++ client/src/Tarefas.js
@@ -1,4 +1,16 @@
 import React, {Component} from "react";
 import axios from "axios";
-import {Card, Header, Form, Input, Icon} from "semantic-ui-react;
+import {Card, Header, Form, Input, Icon} from "semantic-ui-react";
 
+let endpoint = "http://localhost:9000";
+
+class Tarefas extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state ={
+            task:"",
+            items:[],
+        };
+    }
+} 
