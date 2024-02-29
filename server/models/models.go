64db8daa9ff1adab7f5007f0bd3e362b7a39a5d2
kauan <ricardo.kauand@gmail.com>
2024-02-29 12:15:03 -0300

    model config

--- server/models/models.go
+++ server/models/models.go
@@ -1,9 +1,10 @@
-package models
+package models // Declaração do pacote models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import "go.mongodb.org/mongo-driver/bson/primitive" // Importação do pacote para trabalhar com tipos BSON no MongoDB
 
+// Definição da estrutura Tarefas para representar uma tarefa
 type Tarefas struct {
-	ID		primitive.ObjectID		`json:"_id, omitempty" bson: "_id,omitempty"`
-	Task	string					`json:"tarefa,omitempty"`
-	Status 	bool					`json:"status, omitempty"`
+	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"` // Identificador único da tarefa no MongoDB
+	Task   string             `json:"tarefa,omitempty"`                    // Descrição da tarefa
+	Status bool               `json:"status,omitempty"`                    // Status da tarefa (completa ou incompleta)
 }
