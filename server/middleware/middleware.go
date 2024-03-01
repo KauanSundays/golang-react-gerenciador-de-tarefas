640ca61ebcbd015fab3fb2691e2f4a0c43f43d18
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-02-29 21:08:31 -0300

    import and func's

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -1,1 +1,37 @@
 package middleware
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive" // Importação do pacote para trabalhar com tipos BSON no MongoDB
+	"go.mongodb.org/mongo-driver/bson/mongo"
+	"go.mongodb.org/mongo-driver/bson/mongo/options"
+)
+
+
+
+func GetAllTasks(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func CreateTask() {
+
+}
+
+func UndoTask() {
+
+}
+
+func DeleteTask() {
+
+}
+
+func deleteAllTasks() {
+
+}
