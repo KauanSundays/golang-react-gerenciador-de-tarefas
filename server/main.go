1c6b2e56761f92aee35163ba94a8129302d1bf95
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-04-03 19:17:55 -0300

    arquivos novos

---
+++ server/main.go
@@ -0,0 +1,18 @@
+
+package main
+
+import(
+	"fmt"
+	"log"
+	"net/http"
+	"server/router"
+)
+
+func main() {
+	r := router.Router()
+	fmt.Println("Starting server on the port 9000...")
+
+	log.Fatal(http.ListenAndServe(":9000", r))
+	
+
+}
